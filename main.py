935da606b95f1be2d4705cb932e4de33f8d0128e
吹雪かおる <161923118+YuzuAkach@users.noreply.github.com>
2026-01-03 14:31:46 +0000

    Refactor yt-dlp options in download_media function for improved configuration clarity

--- main.py
+++ main.py
@@ -28,9 +28,21 @@ def download_media(url: str, type: str, codec: str):
     # Konfigurasi yt-dlp
     ydl_opts = {
         'outtmpl': output_template,
-        'addmetadata': True,  # Metadata lengkap
-        'writethumbnail': True, # Embed thumbnail
+        'addmetadata': True,
+        'writethumbnail': True,
         'quiet': True,
+        'nocheckcertificate': True,
+        'ignoreerrors': False,
+        'logtostderr': False,
+        'no_warnings': True,
+        'default_search': 'auto',
+        'source_address': '0.0.0.0', # Memaksa penggunaan IPv4
+        'http_headers': {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'en-us,en;q=0.5',
+            'Sec-Fetch-Mode': 'navigate',
+        }
     }
 
     # Logika Video
